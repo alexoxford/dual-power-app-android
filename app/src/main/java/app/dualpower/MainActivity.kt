c23dcd2f6a7129a5543111f8b75c4bfccd480127
Alex Oxford <alex@oakhaven.org>
2020-11-15 11:33:14 -0500

    feat: changes FAB placeholder text

--- app/src/main/java/app/dualpower/MainActivity.kt
+++ app/src/main/java/app/dualpower/MainActivity.kt
@@ -1,11 +1,10 @@
 package app.dualpower
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        // TODO implement submitting a proposal
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            Snackbar.make(view, "Coming soon: propose a Dual Power Project!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
